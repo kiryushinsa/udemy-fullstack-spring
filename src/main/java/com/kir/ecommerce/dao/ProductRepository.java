d4288aa8465fc4a933951f2b2496ee98f34cfb57
kiryushinsa <kiryushin.s.a@gmail.com>
2020-04-12 22:52:51 +0300

    lesson80

--- src/main/java/com/kir/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/kir/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,20 @@
 package com.kir.ecommerce.dao;
 
 import com.kir.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.data.domain.Pageable;
+
+
 
 @CrossOrigin("http://localhost:4200")  // accept calls from web browser for this origin. Need for exclude an error of CORS policy
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable); //behind this equals SELECT * FROM product where category_id=?
+    // spring data rest loc:8080/api/products/searc/findByCategory?id=3 print all from category 3 in database
 
 
 }
